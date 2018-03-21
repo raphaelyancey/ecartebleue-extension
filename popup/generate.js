d5513b3427b853809fb48fe3801e3b195b28559e
Raphaël Yancey <raphael@badfile.net>
2018-03-21 19:35:14 +0100

    Removed axios in favor of jQuery and created the form.

--- popup/generate.js
+++ popup/generate.js
@@ -1,6 +1,31 @@
-function generate() {
-  return axios.get('https://github.com')
-  .then(function(res) {
-    return JSON.parse(res);
-  });
+var API_URL = "http://localhost:3000";
+var credentials = ['FCPRSQBX', 'm3kxkRw7'];
+var p = btoa(JSON.stringify(credentials));
+
+var generateButton = $('button#generate');
+var generatedForm = $('#generated');
+var errorElement = $('#error');
+var ccElement = $('#cc');
+var expElement = $('#exp');
+var ccvElement = $('#ccv');
+
+function pristineState() {
+  errorElement.hide();
 }
+
+generateButton.click(function() {
+  pristineState();
+  $.post(API_URL + '/generate', { p: p })
+  .done(function(res) {
+    console.log('••• res', res);
+    ccElement.val(res.cc);
+    expElement.val(res.exp.join('/'));
+    ccvElement.val(res.ccv);
+  })
+  .fail(function(err) {
+    if(err.error) {
+      errorElement.html(err.error)
+      errorElement.show();
+    } else console.error(err);
+  });
+});
