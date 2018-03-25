735f5c5cb74f7957f8093a0a672fe8f2e644516c
Raphaël Yancey <raphael@badfile.net>
2018-03-25 19:24:57 +0200

    Refreshed the UI

--- popup/generate.js
+++ popup/generate.js
@@ -9,12 +9,27 @@ var ccElement = $('#cc');
 var expElement = $('#exp');
 var ccvElement = $('#ccv');
 
+errorElement.hide();
+
 function pristineState() {
   errorElement.hide();
 }
 
+function loading(state) {
+  if(state) {
+    generateButton.attr('disabled', true);
+    $('.ready', generateButton).hide();
+    $('.loading', generateButton).show();
+  } else {
+    generateButton.attr('disabled', false);
+    $('.ready', generateButton).show();
+    $('.loading', generateButton).hide();
+  }
+}
+
 generateButton.click(function() {
   pristineState();
+  loading(true);
   $.post(API_URL + '/generate', { p: p })
   .done(function(res) {
     ccElement.val(res.cc);
@@ -22,9 +37,13 @@ generateButton.click(function() {
     ccvElement.val(res.ccv);
   })
   .fail(function(err) {
-    if(err.data.error) {
-      errorElement.html(err.data.error)
+    console.error(err);
+    if(err.responseJSON && 'error' in err.responseJSON) {
+      errorElement.html(err.responseJSON.error)
       errorElement.show();
     }
+  })
+  .always(function() {
+    loading(false);
   });
 });
